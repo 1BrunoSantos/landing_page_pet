e71f6e50cf7b10cea5a8858aa26107196226828e
Bruno Santos <brunoasantos88@gmail.com>
2024-09-18 08:56:43 -0300

    Estrutura completa

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menu = document.querySelector('.menu');
+openMenu.addEventListener('click', () => {
+	menu.style.display = "flex"
+	menu.style.right = (menu.offsetWidth * -1) + 'px'
+	setTimeout(()=> {
+		menu.style.opacity = '1'
+		menu.style.right = "0"
+		openMenu.style.display = 'none'
+	}, 10);
+})
 
-    menuToggle.addEventListener('click', function() {
-        menu.classList.toggle('active');
-    });
-});
+closeMenu.addEventListener('click', () => {
+	menu.style.opacity = '0'
+	menu.style.right = (menu.offsetWidth * -1) + 'px'
+	setTimeout(()=> {
+		menu.removeAttribute('style')
+		openMenu.removeAttribute('style')
+	}, 200);
+})
